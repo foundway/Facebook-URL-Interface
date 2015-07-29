c45af1c96270f18f867e87b33e77754b547227c3
Fangwei <fangwei.lee@gmail.com>
2015-07-29 09:01:54 -0700

    update

---
+++ Assets/Main.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	private readonly string FBURL = "http://m.facebook.com/";
+	public string PageID;
+	public string AppID;
+	public string Link;
+	public string Name;
+	public string Caption;
+	public string Picture;
+	public string Redirect;
+	public string Description;
+
+	public void OnLike() {
+		Application.OpenURL(FBURL + PageID);
+	}
+
+	public void OnFeed() {
+		Application.OpenURL (FBURL + "/dialog/feed?app_id=" + AppID +
+			                     "&link=" + WWW.EscapeURL(Link) +
+			                     "&name=" + WWW.EscapeURL(Name) +
+			                     "&caption=" + WWW.EscapeURL(Caption) + 
+			                     "&description=" + WWW.EscapeURL(Description) + 
+			                     "&picture=" + WWW.EscapeURL(Picture) + 
+			                     "&redirect_uri=" + WWW.EscapeURL(Redirect));
+	}
+}
